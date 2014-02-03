a4c1337c449ade90099194bdc9f52f91bf84f863
David Ascher <david.ascher@gmail.com>
2014-02-03 08:46:19 -0800

    envvarify

--- config/secrets.js
+++ config/secrets.js
@@ -2,10 +2,8 @@ module.exports = {
   db: process.env.MONGOLAB_URI || process.env.MONGO_URI || 'mongodb://localhost/kitchen',
 
   github: {
-    // clientID: 'a78350cbc8e679e9c291',
-    // clientSecret: '81a4e74890d4b68f9ac70885f3ce5613cf7ef8f5',
-    clientID: '542b0ba05f2226c15ba4',
-    clientSecret: 'f72d61e610c0d28ece706262aa7f548e9e8a7e5c',
+    clientID: process.env.GITHUB_CLIENTID,
+    clientSecret: process.env.GITHUB_CLIENTSECRET,
     callbackURL: '/auth/github/callback',
     passReqToCallback: true
   },
